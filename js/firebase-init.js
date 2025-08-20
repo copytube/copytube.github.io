ac54856349273f727be172583b1df152693e39cf
copytube <elexpecto@gmail.com>
2025-08-20 14:07:58 +0900

    Update firebase-init.js
    new

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,7 +1,15 @@
+// js/firebase-init.js
+// Firebase SDK v12.x (CDN 모듈) 초기화
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+/**
+ * ⚠️ 프로젝트 SDK 설정값으로 교체 가능.
+ * 사용자가 공유한 설정(예시):
+ * projectId: copytube-daf30
+ */
 const firebaseConfig = {
   apiKey: "AIzaSyBdZwzeAB91VnR0yqZK9qcW6LsOdCfHm8U",
   authDomain: "copytube-daf30.firebaseapp.com",
